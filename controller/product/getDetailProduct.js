82d6e0db03019144d9e08293f056bbb650ba1954
unknown <zaenalblaze@gmail.com>
2021-03-19 00:10:04 +0700

    getDetailProduct

--- controller/product/getDetailProduct.js
+++ controller/product/getDetailProduct.js
@@ -1,5 +1,38 @@
-const { Product } = require("../../models");
+const db = require("../../models");
+const Product = db.product;
+const User = db.user;
 
 module.exports = async (req, res) => {
- 
+  const { productId } = req.params;
+
+  try {
+    const product = await Product.findOne({
+      where: {
+        id: productId,
+      },
+      attributes: ["id", "title", "price", "image"],
+
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "fullName", "email", "phone", "location"],
+        },
+      ],
+    });
+
+    if (product == null) {
+      return res.status(404).json({
+        status: "not found",
+        message: "product not found",
+      });
+    }
+
+    return res.json({
+      status: "success",
+      data: {
+        product: product,
+      },
+    });
+  } catch (error) {}
 };
