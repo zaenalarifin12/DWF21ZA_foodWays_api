eada4a131448f5a04fea9ce1880d096b65177fbe
unknown <zaenalblaze@gmail.com>
2021-03-19 00:03:08 +0700

    add product by partner

--- controller/product/getProductByPartner.js
+++ controller/product/getProductByPartner.js
@@ -1,3 +1,20 @@
-module.exports = () => {
-    
-}
+const db = require("../../models");
+const Product = db.product;
+
+module.exports = async (req, res) => {
+  const { userId } = req.params;
+
+  const products = await Product.findAll({
+    where: {
+      userId: userId,
+    },
+    attributes: ["id", "title", "price", "image"],
+  });
+
+  return res.json({
+    status: "success",
+    data: {
+      products: products,
+    },
+  });
+};
