471e4a9f12908a3e5dcbcf5ac85d3c593de6a97b
unknown <zaenalblaze@gmail.com>
2021-03-29 05:12:12 +0700

    edit ssl

--- migrations/20210318143332-create-product.js
+++ migrations/20210318143332-create-product.js
@@ -21,7 +21,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'products', // name of Target model
+          model: 'users', // name of Target model
           key: 'id', // key in Target model that we're referencing
         },
         onUpdate: 'CASCADE',
